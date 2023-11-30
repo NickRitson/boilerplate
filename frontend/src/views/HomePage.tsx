03eb6382c85e3d1b2dc3a3145736a0ad04561d24
Nick Ritson <nritsonsa@gmail.com>
2023-11-30 10:42:35 +0000

    testing only branch

--- frontend/src/views/HomePage.tsx
+++ frontend/src/views/HomePage.tsx
@@ -3,13 +3,11 @@ import React from 'react';
 const HomePage: React.FC = () => {
     return (
         <div className="bg container mx-auto">
-            <h1 className="text text-6xl font-bold text-center mt-8">
+            <h1 className="text-6xl font-bold text-center mt-8">
                 React <span className="bg-gradient-to-r from-lime-400 to-emerald-400 bg-clip-text text-transparent">Boilerplate</span> and <span className="bg-gradient-to-r from-cyan-400 to-indigo-400 bg-clip-text text-transparent">Playground</span>
             </h1>
             <div className="flex justify-center my-8">
-                <button className="rounded py-4 px-8 bg-gray-300 border-gray-50 text-gray-700 font-bold">
-                    Start Coding
-                </button>
+                Vite - React - TailwindCSS - Redux - TypeScript - ESLint - Jest
             </div>
         </div>
     );
--- frontend/src/views/TechStackPage.tsx
+++ frontend/src/views/TechStackPage.tsx
@@ -1,7 +1,18 @@
 const TechStackPage: React.FC = () => {
     return (
-        <div>
-            TECH STACKS
+        <div className="bg container mx-auto">
+            <h1 className="text-6xl font-bold">TECH STACKS</h1>
+            <u>Currently Added:</u>
+            <ul>
+                <li>Vite: </li>
+                <li>React: </li>
+                <li>TailwindCSS: </li>
+                <li>Redux: </li>
+                <li>TypeScript: </li>
+                <li>Jest: </li>
+                <li>Vercel: </li>
+                <li>CI (Lighthouse, Embold): </li>
+            </ul>
         </div>
     );
 }
