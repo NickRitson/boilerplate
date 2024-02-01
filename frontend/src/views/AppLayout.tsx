11ba251e66ef56a63217c400f52543b2e47963a0
Nicholas Ritson <nicholasritson@Nicholass-MacBook-Pro-2.local>
2024-02-01 03:04:38 +0000

    testing

--- frontend/src/views/AppLayout.tsx
+++ frontend/src/views/AppLayout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 
 import Navbar from '../components/navbar/Navbar';
-import Footer from '../components/footer/Footer';
+// import Footer from '../components/footer/Footer';
 
 const AppLayout: React.FC = () => {
     return (
@@ -10,7 +10,7 @@ const AppLayout: React.FC = () => {
         <div>
           <Outlet />
         </div>
-        <Footer />
+        {/* <Footer /> */}
       </div>
     );
   }
